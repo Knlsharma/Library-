0c92d62fea19691f831e3427c83cca5a221b7bbf
Knlsharma <knlsharma678@gmail.com>
2020-01-11 23:02:27 +0530

    removed old skeleton

--- locallibrary/login/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Login(models.Model):
-     
-     name = models.CharField(max_length=20)
-     passw = models.CharField(max_length=10)
-
