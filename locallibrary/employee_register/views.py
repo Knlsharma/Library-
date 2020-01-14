146867969ee4e4b4d47b0731876b6b0b274d2c22
Knlsharma <knlsharma678@gmail.com>
2020-01-14 23:35:11 +0530

    final upload

--- locallibrary/employee_register/views.py
+++ locallibrary/employee_register/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render, redirect
 from .forms import EmployeeForm
 from .models import Employee
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
-@login_required
+
 def employee_list(request):
     context = {'employee_list': Employee.objects.all()}
     return render(request, "employee_register/employee_list.html", context)
 
 
-@login_required
+
 def employee_form(request, id=0):
     if request.method == "GET":
         if id == 0:
@@ -35,6 +37,29 @@ def employee_delete(request,id):
     employee = Employee.objects.get(pk=id)
     employee.delete()
     return redirect('/home/book/list')
+
+def search(request):
+    if request.method=='POST':
+        srch = request.POST['srh']
+        print(srch)
+        if srch:
+            match = Employee.objects.filter( Q(Bookname__icontains=srch) |
+                                         Q(Authorname__icontains=srch)                                                   
+                                                   )
+
+            print(match)   
+
+            form = EmployeeForm(instance=match)                                      
+
+            if match:
+                return render(request, "employee_register/search.html", {'form': form})
+            else:
+                messages.error(request , 'no result found')
+
+        else:
+            return redirect('/home/book/search/')        
+                    
+    return render(request ,'employee_register/search.html')                                                 
 
 
     
